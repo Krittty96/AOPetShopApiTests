edd0813e14e005b141963320e614a841c85a8cb2
Krit <kritkitkit@gmail.com>
2026-01-23 00:51:54 +0700

    Добавлиа тесты: "Добавление нового питомца" и ""Добавление нового питомца с полными данными"

--- Tests/test_pet.py
+++ Tests/test_pet.py
@@ -1,18 +1,21 @@
 import allure
+import jsonschema
 import requests
+from .schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = 'http://5.181.109.28:9090/api/v3'
+
 
 @allure.feature('Pet')
 class TestPet:
     @allure.title('Попытка удалить несуществующего питомца')
     def test_delete_nonexistent_pet(self):
         with allure.step('Отправка запроса на удаление несуществующего питомца'):
-            response = requests.delete(url= f'{BASE_URL}/pet/9999')
+            response = requests.delete(url=f'{BASE_URL}/pet/9999')
         with allure.step('Проверка статуса ответа'):
             assert response.status_code == 200, 'Код ответа не совпал с ожидаемым'
         with allure.step('Проверка текстового содержимого ответа'):
-            assert response.text == 'Pet deleted' , 'Текст ошибки не совпал с ожидаемым'
+            assert response.text == 'Pet deleted', 'Текст ошибки не совпал с ожидаемым'
 
     @allure.title('Попытка обновить несуществующего питомца')
     def test_update_nonexistent_pet(self):
@@ -22,7 +25,7 @@ class TestPet:
                 "name": "Non-existent Pet",
                 "status": "available"
             }
-            response = requests.put(url= f'{BASE_URL}/pet', json=payload)
+            response = requests.put(url=f'{BASE_URL}/pet', json=payload)
         with allure.step('Проверка статуса ответа'):
             assert response.status_code == 404, 'Код ответа не совпал с ожидаемым'
         with allure.step('Проверка текстового содержимого ответа'):
@@ -31,8 +34,78 @@ class TestPet:
     @allure.title('Попытка получить информацию о несуществующем питомце')
     def test_get_nonexistent_pet(self):
         with allure.step('Отправка запроса на получение информации о несуществующем питомце'):
-            response = requests.get(url= f'{BASE_URL}/pet/9999')
+            response = requests.get(url=f'{BASE_URL}/pet/9999')
         with allure.step('Проверка статуса ответа'):
             assert response.status_code == 404, 'Код ответа не совпал с ожидаемым'
         with allure.step('Проверка текстового содержимого ответа'):
-            assert response.text == 'Pet not found' , 'Текст ошибки не совпал с ожидаемым'
+            assert response.text == 'Pet not found', 'Текст ошибки не совпал с ожидаемым'
+
+    @allure.title('Добавление нового питомца')
+    def test_add_pet(self):
+        with allure.step('Подготовка данных для создания питомца'):
+            payload = {
+                "id": 1,
+                "name": "Buddy",
+                "status": "available"
+            }
+        with allure.step('Отправка запроса на создание питомца'):
+            response = requests.post(url=f'{BASE_URL}/pet', json=payload)
+            response.json = response.json()
+
+        with allure.step('Проверка статуса ответа и валидация JSON-схемы'):
+            assert response.status_code == 200, 'Код ответа не совпал с ожидаемым'
+            jsonschema.validate(response.json, PET_SCHEMA)
+
+        with allure.step('Проверка параметров питомца в ответе'):
+            assert response.json['id'] == payload['id'], 'id питомца не совпадает с ожидаемым'
+            assert response.json['name'] == payload['name'], 'name питомца не совпадает с ожидаемым'
+            assert response.json['status'] == payload['status'], 'status питомца не совпадает с ожидаемым'
+
+    @allure.title('Добавление нового питомца с полными данными')
+    def test_add_pet(self):
+        with allure.step('Подготовка данных для создания питомца'):
+            payload = {
+                "id": 10,
+                "name": "doggie",
+                "category": {
+                    "id": 1,
+                    "name": "Dogs"
+                },
+                "photoUrls": ["string"],
+                "tags": [
+                    {
+                        "id": 0,
+                        "name": "string"
+                    }
+                ],
+                "status": "available"
+            }
+            with allure.step('Отправка запроса на создание питомца'):
+                response = requests.post(url=f'{BASE_URL}/pet', json=payload)
+                response.json = response.json()
+
+            with allure.step('Проверка статуса ответа и валидация JSON-схемы'):
+                assert response.status_code == 200, 'Код ответа не совпал с ожидаемым'
+                jsonschema.validate(response.json, PET_SCHEMA)
+
+            with allure.step('Проверка параметров питомца в ответе'):
+                assert response.json['id'] == payload['id'], 'id питомца не совпадает с ожидаемым'
+                assert response.json['name'] == payload['name'], 'name питомца не совпадает с ожидаемым'
+                assert response.json['category']['id'] == payload['category'][
+                    'id'], 'id category не совпадает с ожидаемым'
+                assert response.json['category']['name'] == payload['category'][
+                    'name'], 'name category не совпадает с ожидаемым'
+                assert response.json['photoUrls'] == payload['photoUrls'], 'photoUrls не совпадает с ожидаемым'
+                assert response.json['tags'][0]['id'] == payload['tags'][0]['id'], 'Id тега не совпадает с ожидаемым'
+                assert response.json['tags'][0]['name'] == payload['tags'][0][
+                    'name'], 'Наименования тега не совпадает с ожидаемым'
+                assert response.json['status'] == payload['status'], 'status питомца не совпадает с ожидаемым'
+                # Второй вариант реализации проверки значений внутри массива "tags":
+                if len(response.json['tags']) == len(payload['tags']):
+                    for i in range(len(response.json['tags'])):
+                        if response.json['tags'][i]['id'] != payload['tags'][i]['id']:
+                            print('Id тега не совпадает с ожидаемым')
+                        if response.json['tags'][i]['name'] != payload['tags'][i]['name']:
+                            print('Name тега не совпадает с ожидаемым')
+                else:
+                    print('Содержимое тега не совпадает с ожидаемым')
