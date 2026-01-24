56f06480f0985722fca8929236cfeaf3ceac8980
Krit <kritkitkit@gmail.com>
2026-01-24 17:19:57 +0700

    исправление отступов в коде

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -3,13 +3,14 @@ import requests
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
+
 @pytest.fixture(scope='function')
 def create_pet():
-            payload = {
-                "id": 1,
-                "name": "Buddy",
-                "status": "available"
-            }
-            response = requests.post(url=f'{BASE_URL}/pet', json=payload)
-            assert response.status_code == 200
-            return response.json()
+    payload = {
+        "id": 1,
+        "name": "Buddy",
+        "status": "available"
+    }
+    response = requests.post(url=f'{BASE_URL}/pet', json=payload)
+    assert response.status_code == 200
+    return response.json()
